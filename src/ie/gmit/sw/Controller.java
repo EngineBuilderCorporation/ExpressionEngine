28f2b425353d734f642d82674461c131528d49d1
Ross Byrne <ross.byrne@outlook.ie>
2017-02-26 17:06:12 +0000

    Cleaned up more code, ScrollPane is now working correctly

--- src/ie/gmit/sw/Controller.java
+++ src/ie/gmit/sw/Controller.java
@@ -35,57 +35,16 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        sp.setHmax(440);
-        sp.setPrefSize(600, 100);
+        sp.setPrefSize(700, 100);
 
-        int newColIndex = 1;
-
-
-        // add a column in at a certain index
-        try {
-
-            // loop through each column, starting at the end (right hand side)
-            // loop until selected index, shifting to right, by one index
-            for(int i = grid.getColumnConstraints().size() - 1; i >= newColIndex; i--){
-
-                // get node from column
-                Node n = getNodeByColumnIndex(new Integer(i), grid);
-
-                // remove node from children to avoid dupes (throws exception otherwise)
-                grid.getChildren().remove(n);
-
-                // add node to next column to shift its index up by one
-                grid.add(n, i + 1, 0);
-
-            } // for
-
-            // create new column constraint
-            ColumnConstraints col = new ColumnConstraints(200);
-
-            // add it to the new end column in grid
-            grid.getColumnConstraints().addAll(col);
-
-            // insert new node into selected index
-            grid.add(new Label("Beans"), newColIndex, 0);
-
-            // update the width of each column
-            for(int i = 0; i < grid.getColumnConstraints().size(); i++){
-
-                // get column constraint
-                ColumnConstraints columnConstraints = grid.getColumnConstraints().get(i);
-
-                // set the columns width
-                columnConstraints.setPrefWidth(100);
-
-                // center the contents of column
-                columnConstraints.setHalignment(HPos.CENTER);
-
-            } // for
-
-        } catch (Exception e){
-
-            e.printStackTrace();
-        } // try
+        // add new columns
+        addColumn(0, new Label("New 1"));
+        addColumn(1, new Label("New 2"));
+        addColumn(2, new Label("New 3"));
+        addColumn(3, new Label("New 4"));
+        addColumn(4, new Label("New 5"));
+        addColumn(5, new Label("New 6"));
+        addColumn(6, new Label("New 7"));
 
         // set the options for the operators comboBox
         operator.setItems(FXCollections.observableArrayList(
@@ -106,15 +65,79 @@ public class Controller implements Initializable {
 
             Label l = new Label("Hello");
 
-            // first remove old contents from children
-            grid.getChildren().remove(getNodeByColumnIndex(new Integer(3), grid));
+//            Node n = l;
+//
+//            if(n instanceof Label){
+//
+//                System.out.println("Hot dog, it's a Label");
+//            }
 
-            // add the new node
-            grid.add(l, 3, 0);
+            // add a new column at index 4
+            addColumn(4, l);
 
         });
 
     } // initialize()
+
+    // adds a column at selected index with provided node
+    public void addColumn(int index, Node node){
+
+        // add a column in at a certain index
+        try {
+
+            // loop through each column, starting at the end (right hand side)
+            // loop until selected index, shifting to right, by one index
+            for(int i = grid.getColumnConstraints().size() - 1; i >= index; i--){
+
+                // get node from column
+                Node n = getNodeByColumnIndex(new Integer(i), grid);
+
+                // remove node from children to avoid dupes (throws exception otherwise)
+                grid.getChildren().remove(n);
+
+                // add node to next column to shift its index up by one
+                grid.add(n, i + 1, 0);
+
+            } // for
+
+            // create new column constraint
+            ColumnConstraints col = new ColumnConstraints(200);
+
+            // add it to the new end column in grid
+            grid.getColumnConstraints().addAll(col);
+
+            // insert new node into selected index
+            grid.add(node, index, 0);
+
+            double gridSize = 0;
+
+            // update the width of each column
+            for(int i = 0; i < grid.getColumnConstraints().size(); i++){
+
+                // get column constraint
+                ColumnConstraints columnConstraints = grid.getColumnConstraints().get(i);
+
+                // set the columns width
+                columnConstraints.setPrefWidth(100);
+                columnConstraints.setHgrow(Priority.NEVER);
+
+                // center the contents of column
+                columnConstraints.setHalignment(HPos.CENTER);
+
+                // calculate the size of the grid
+                gridSize += columnConstraints.getPrefWidth();
+
+            } // for
+
+            // update the width of the grid
+            grid.setPrefWidth(gridSize);
+
+        } catch (Exception e){
+
+            e.printStackTrace();
+        } // try
+
+    } // addColumn()
 
     // gets a node by its index from a gridpane
     public Node getNodeByColumnIndex (final Integer column, GridPane gridPane) {
